c9d90430c09d0ebe2c9baebc08f9583a9cf9ce95
nelsonGX <hi@nelsongx.com>
2025-02-25 23:21:39 +0800

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,5 +2,5 @@ import { LOG_MAJOR } from '../util/log_util';
 import { startServer } from './server';
 
 // Start the API server
-LOG_MAJOR("Starting API server...");
+LOG_MAJOR('Starting API server...');
 startServer();
